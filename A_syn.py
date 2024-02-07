789c5a5fcc30637245b7da77045cb8c652b7056e
Joana <asiakom24@vp.pl>
2024-02-07 19:31:05 +0100

    .

--- A_syn.py
+++ A_syn.py
@@ -48,8 +48,8 @@ for chunk_size_id, chunk_size in enumerate(chunk_sizes):
                         temp = mean_res[training_int_id, chunk_size_id, n_f_id, y_noise_id, n_d_id, method_id]
                         aa.plot(gaussian_filter1d(temp, 3), color=cols[method_id], label=method, linewidth=0.75)
                     
-                    l = get_real_drift(1000, n_d).astype(int)-1
-                    aa.set_xticks(l, l, rotation=90)
+                    l = get_real_drift(1000, n_d).astype(int)
+                    aa.set_xticks(l-1, l, rotation=90)
                     aa.grid()
                     aa.spines['top'].set_visible(False)
                     aa.spines['right'].set_visible(False)
