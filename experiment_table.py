3c11d05f28f44508b874f66ede72fe31db2d7e4a
Paweł Ksieniewicz <pawel.ksieniewicz@pwr.edu.pl>
2024-10-25 17:05:18 +0200

    Revision of figures.

--- experiment_table.py
+++ experiment_table.py
@@ -17,15 +17,14 @@ dets = ['Oracle', 'Real']
 results = np.load('results/results.npy')
 # reps, deltas, frameworks, classifiers, drifts, detectors, chunks, (bac, detections, trainings)
 
-
 m_accs = np.nanmean(results[...,0], axis=(-1,0))
 m_requests = (np.mean(np.sum((np.isnan(results[...,1])==False), axis=-1), axis=0)/n_chunks)
 m_trainings = (np.mean(np.sum((np.isnan(results[...,2])==False), axis=-1), axis=0)/n_chunks)
 
-print(m_accs.shape) # deltas, frameworks, classifiers, drifts, detectors
-print(m_requests.shape)
-print(m_requests[0,:,0,0])
-print(m_trainings.shape)
+# print(m_accs.shape) # deltas, frameworks, classifiers, drifts, detectors
+# print(m_requests.shape)
+# print(m_requests[0,:,0,0])
+# print(m_trainings.shape)
 
 
 rows = []
@@ -66,8 +65,29 @@ tab_data = [r[1:] for r in tab_data]
 tab_data = np.array(tab_data)
 print(tab_data.shape)
 
-#fig, ax = plt.subplots(1,3, figsize=(15,15), sharex=True, sharey=True)
-fig, ax = plt.subplots(4,3, figsize=(15,15), sharex=True, sharey=False)
+"""
+HERE COMES THE SUN
+"""
+fig, ax = plt.subplots(4,4, figsize=(9,9*1.618), sharex=False, sharey=False,
+                       width_ratios=[1,1,1,.4])
+
+
+# Right legend
+for z in range(4):
+    ax[z,-1].set_ylim(-.5,11.5)
+
+    ax[z, -1].tick_params(top=True, labeltop=True, bottom=False, labelbottom=False)
+    ax[z, -1].set_xlim(-.5, 1.5)
+    ax[z, -1].spines['right'].set_visible(False)
+    ax[z, -1].set_xticks([0,1], ['$\delta$', 'Drifts'])
+    ax[z, -1].set_yticks([3.5,7.5],['',''])
+    ax[z, -1].grid(ls=":", axis='y')
+
+
+    for i, v_drifts in enumerate([5,10,15]):
+        ax[z, -1].text(1,9.5-i*4,v_drifts, ha='center', va='center')
+        for j, v_delta in enumerate([1,10,20,60]):        
+            ax[z, -1].text(0,11-(j+i*4),v_delta, ha='center', va='center')
 
 rows_per_framework = 12
 for framework_id in range(4):
@@ -86,39 +106,36 @@ for framework_id in range(4):
                 vmin=0, vmax=0.04)
     
     if framework_id == 0:
-        ax[framework_id, 0].set_title('Balanced Accuracy', fontsize=15)
-        ax[framework_id, 1].set_title('Label request in chunk', fontsize=15)
-        ax[framework_id, 2].set_title('Training in chunk', fontsize=15)
+        ax[framework_id, 0].set_title('Balanced Accuracy')
+        ax[framework_id, 1].set_title('Label request in chunk')
+        ax[framework_id, 2].set_title('Training in chunk')
 
 # continous
 for _a in range(12):
     for _b in range(3):
-        ax[0, 0].text(2*_b+0.5, _a, "%.3f" % (
+        ax[0, 0].text(2*_b+0.5, _a, ("%.3f" % (
             tab_data[_a, 2*_b]
-            ) , va='center', ha='center', 
-                c='white' if (tab_data[_a, 2*_b] >0.75) or (tab_data[_a, 2*_b]<0.55) else 'black', 
-                fontsize=11)
+            ))[1:] , va='center', ha='center', 
+                c='white' if (tab_data[_a, 2*_b] >0.75) or (tab_data[_a, 2*_b]<0.55) else 'black')
 
 # remaining
 for _a in range(12,48):
     for _b in range(6):
         framework_id = _a // rows_per_framework
-        print(framework_id, _a, _b)
+        # print(framework_id, _a, _b)
         
-        ax[framework_id, 0].text(_b, _a%rows_per_framework, tab_data[_a, _b],
+        ax[framework_id, 0].text(_b, _a%rows_per_framework, ('%.3f' %tab_data[_a, _b])[1:],
                                  ha='center', va='center',
-                                 c='white' if (tab_data[_a, _b] >0.75) or (tab_data[_a, _b]<0.55) else 'black',
-                                 fontsize=11)
+                                 c='white' if (tab_data[_a, _b] >0.75) or (tab_data[_a, _b]<0.55) else 'black')
 
 # continous
 for _a in range(12):
     for _b in range(3):
         aa = 6
-        ax[0, 1].text(2*_b+0.5, _a, "%.3f" % (
+        ax[0, 1].text(2*_b+0.5, _a, ("%.3f" % (
             tab_data[_a, 2*_b+aa]
-            ) , va='center', ha='center', 
-                   c='white' if (tab_data[_a, 2*_b+aa] > 0.75) else 'black', 
-                   fontsize=11)
+            ))[1:] , va='center', ha='center', 
+                   c='white' if (tab_data[_a, 2*_b+aa] > 0.75) else 'black')
 
 # remaining
 for _a in range(12,48):
@@ -126,11 +143,10 @@ for _a in range(12,48):
         framework_id = _a // rows_per_framework
         
         aa = 6
-        ax[framework_id, 1].text(_b, _a%rows_per_framework, "%.3f" % (
+        ax[framework_id, 1].text(_b, _a%rows_per_framework, ("%.3f" % (
             tab_data[_a, _b + aa]
-            ) , va='center', ha='center', 
-                   c='white' if (tab_data[_a, _b+aa] > 0.75) else 'black', 
-                   fontsize=11)
+            ))[1:] , va='center', ha='center', 
+                   c='white' if (tab_data[_a, _b+aa] > 0.75) else 'black')
 
 
 
@@ -138,11 +154,10 @@ for _a in range(12,48):
 for _a in range(12):
     for _b in range(3):
         aa = 12
-        ax[0, 2].text(2*_b+0.5, _a, "%.3f" % (
+        ax[0, 2].text(2*_b+0.5, _a, ("%.3f" % (
             tab_data[_a, 2*_b+aa]
-            ) , va='center', ha='center', 
-                   c='white',
-                   fontsize=11)
+            ))[1:] , va='center', ha='center', 
+                   c='white')
 
 # remaining
 for _a in range(12,48):
@@ -150,15 +165,18 @@ for _a in range(12,48):
         framework_id = _a // rows_per_framework
         
         aa = 12
-        ax[framework_id, 2].text(_b, _a%rows_per_framework, "%.3f" % (
+        
+        v = "%.3f" % (
             tab_data[_a, _b + aa]
-            ) , va='center', ha='center', 
-                   c='black', 
-                   fontsize=11)
+        )
+        v = v[1:]
+        
+        ax[framework_id, 2].text(_b, _a%rows_per_framework, v , va='center', ha='center', 
+                   c='black')
 
 for i, raa in enumerate(ax):
-    for j, aa in enumerate(raa):
-        aa.set_xticks(np.arange(6), [r[4:] for r in rows[0][1:7]], rotation=90, fontsize=15)
+    for j, aa in enumerate(raa[:-1]):
+        aa.set_xticks(np.arange(6), [r[4:] for r in rows[0][1:7]], rotation=90)
         # aa.set_yticks(np.arange(len(env_names)), env_names, fontsize=15)
         
         start = i*rows_per_framework
@@ -166,9 +184,9 @@ for i, raa in enumerate(ax):
         
         _env_names = env_names[start:stop]
         
-        print(_env_names)
+        # print(_env_names)
         
-        print('A', i, j, len(env_names), start, stop, len(_env_names))
+        # print('A', i, j, len(env_names), start, stop, len(_env_names))
         
         # aa.set_yticks(np.arange(len(_env_names)), _env_names, fontsize=15)
         
@@ -178,12 +196,37 @@ for i in range(4):
     
     _env_names = env_names[start:stop]
     
-    print(i, _env_names)
+    # print(i, _env_names)
     
-    ax[i,0].set_yticks(np.arange(len(_env_names)), _env_names, fontsize=15)
+    # ax[i,0].set_yticks(np.arange(len(_env_names)), _env_names, fontsize=15)
 
-plt.tight_layout()
+ax[0,0].set_ylabel('\nContinous Rebuild')
+ax[1,0].set_ylabel('Triggered Rebuild\nSupervised drift detection')
+ax[2,0].set_ylabel('Triggered Rebuild\nUnsupervised drift detection')
+ax[3,0].set_ylabel('Triggered Rebuild\nSemi-supervised drift detection')
+
+
+for i in range(4):
+    for j in range(3):
+        # ax[i,j].spines['top'].set_visible(False)
+        # ax[i,j].spines['right'].set_visible(False)
+        # ax[i,j].spines['left'].set_visible(False)
+        # ax[i,j].spines['bottom'].set_visible(False)
+        
+        ax[i,j].set_yticks([])
+        
+        ax[i,j].hlines([3.5,7.5], -.5, 5.5, lw=1, color='white', alpha=.5)
+        
+        ax[i,j].vlines([1.5, 3.5], -.5, 11.5, lw=1, color='white', alpha=.5)
+        
+        if i < 3:
+            ax[i,j].set_xticks([])
+
+#plt.tight_layout()
+
+plt.subplots_adjust(left=0.05, right=1., top=0.95, bottom=0.05)
+
 plt.savefig('tables/table_vis.png') 
 plt.savefig('tables/table_vis.eps') 
-plt.savefig('foo.png') 
+plt.savefig('foo.png', dpi = 500)
             
--- experiment_vis.py
+++ experiment_vis.py
@@ -24,43 +24,62 @@ results = np.load('results/results.npy')
 
 print(results.shape)
         
+mag = 2
 
 for rep in range(3):
     for clf_id, clf in enumerate(clfs):
         for n_d_id, n_d in enumerate(drifts):
             for det_id, det in enumerate(dets):
 
-                fig, ax = plt.subplots(4,4, figsize=(20,8), sharex=True, sharey=True)
-                plt.suptitle('CLF: %s | DET: %s | D: %i | R: %i' % (clf, det, n_d, rep), fontsize=15)
+                fig, ax = plt.subplots(4,4, figsize=(4.5*mag,mag*4.5/1.618), sharex=True, sharey=True)
+                
+                title = '%s classifier with %s detector on data stream with %i concept drifts' % (clf, det, n_d)
+                
+                plt.suptitle(title)
                 
                 for d_id, d in enumerate(deltas):
                     for f_id, f in enumerate(frameworks):
                     
-                        ax[f_id, d_id].vlines(results[rep, d_id, f_id, clf_id, n_d_id, det_id, :, 1], 0, 1, label = 'requests', color='tomato', alpha=1, ls='--')
-                        ax[f_id, d_id].vlines(results[rep, d_id, f_id, clf_id, n_d_id, det_id, :, 2],0, 1, label = 'training', color='cornflowerblue', alpha=1, ls=':')
+                        ax[f_id, d_id].vlines(
+                            results[rep, d_id, f_id, clf_id, n_d_id, det_id, :, 1], 
+                            0, 
+                            1, 
+                            label = 'request moments', 
+                            color='tomato', lw=1)
+                        ax[f_id, d_id].vlines(
+                            results[rep, d_id, f_id, clf_id, n_d_id, det_id, :, 2],
+                            0, 
+                            1, 
+                            label = 'trainin moments', color='cornflowerblue', alpha=1, ls=':', lw=1)
 
-                        ax[f_id, d_id].plot(np.arange(1,500),
+                        ax[f_id, d_id].plot(
+                            np.arange(1,500),
                             results[rep, d_id, f_id, clf_id, n_d_id, det_id, :, 0], 
-                            label = 'bac', color='b')
+                            label = 'bac', 
+                            color='k', lw=1)
                                 
                         ax[f_id, d_id].grid(ls=':')
-                        ax[f_id, d_id].set_xticks(get_real_drift(n_chunks, n_d).astype(int))
+                        ax[f_id, d_id].set_xticks(
+                            get_real_drift(n_chunks, n_d).astype(int))
 
                         if f_id==0:
-                            ax[f_id, d_id].set_title('delta = %i' % (d), fontsize=15)
+                            ax[f_id, d_id].set_title('delta = %i' % (d))
                             ax[-1, d_id].set_xlabel('chunk')
                         if d_id==0:
-                            ax[f_id, d_id].set_ylabel(f, fontsize=15)
+                            ax[f_id, d_id].set_ylabel(f)
                             
                         ax[f_id, d_id].spines['top'].set_visible(False)
                         ax[f_id, d_id].spines['right'].set_visible(False)
                         ax[f_id, d_id].set_xlim(0,n_chunks)
 
 
+                handles, labels = ax[-1,-1].get_legend_handles_labels()
+                fig.legend(handles, labels, loc='lower center', ncol=3, frameon=False)
                         
-                ax[-1,-1].legend(frameon=False, ncols=3)
-                plt.tight_layout()
-                plt.savefig('foo.png')
+                plt.subplots_adjust(left=0.1, right=0.9, top=0.85, bottom=0.15)
+                # ax[-1,-1].legend(frameon=False, ncols=3)
+                # plt.tight_layout()
+                plt.savefig('foo.png', dpi=500)
                 plt.savefig('fig_frameworks/acc_%s_%s_%sd_%i.png' % (clf, det, n_d, rep))
                 plt.savefig('fig_frameworks/acc_%s_%s_%sd_%i.eps' % (clf, det, n_d, rep))
 
