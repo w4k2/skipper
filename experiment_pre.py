eedb9ab107d329a6b03b4e44dc83ef0580580be3
Joana <asiakom24@vp.pl>
2024-11-08 17:01:41 +0100

    preliminary cleanup

--- experiment_pre.py
+++ experiment_pre.py
@@ -13,7 +13,6 @@ rs = np.random.randint(10,100000,n_reps).astype(int)
 
 n_chunks = 100
 chunk_size = 500
-
 acc = np.zeros((n_reps, n_chunks-1))
 dist_X0 = np.zeros((n_reps, n_chunks-1))
 dist_X1 = np.zeros((n_reps, n_chunks-1))
@@ -45,47 +44,6 @@ for n in tqdm(range(n_reps)):
 mean_acc = np.mean(acc, axis=0)
 std_acc = np.std(acc, axis=0)
 
-# PLOT
-
-fig, [ax, aa] = plt.subplots(2, 1, figsize=(8,8))
-
-# accuracy 
-# Tutaj rysujemy średni wynik jakości klasyfikacji za pomocą klasyfikatora GNB, uczonego inkrementalnie,
-# pokazujemy że najpierw jakość spada dopiero po jakimś czasie od wystąpienia zmian
-
-ax.fill_between(np.arange(n_chunks-1), 
-                mean_acc-std_acc, mean_acc+std_acc, 
-                alpha=0.3, color='red',
-                lw=0)
-ax.plot(mean_acc, color='red')
-
-ax2 = ax.twinx()
-ax2.plot(stream.concept_probabilities[::chunk_size], color='black', ls=':')
-
-ax.spines['top'].set_visible(False)
-ax2.spines['top'].set_visible(False)
-
-ax.spines['right'].set_visible(False)
-ax2.spines['right'].set_visible(False)
-ax2.set_yticks([])
-
-ax.grid(ls=':')
-ax.set_xlim(0,99)
-
-ax.set_xlabel('chunk')
-ax.set_ylabel('accuracy')
-
-handles, labels = plt.gca().get_legend_handles_labels()
-line1 = Line2D([0], [0], label='accuracy', color='red')
-line2 = Line2D([0], [0], label='concept probability', color='black', ls=':')
-handles.extend([line1, line2])
-ax.legend(handles=handles, frameon=False)
-
-
-# centroids
-# Tutaj rysujemy średnią odległość aktualnego centroidu klas (problem binarny) od początkowego centroidu
-# pokazujemy że zmiany w rozkładzie (przesunięcie centroidów w przestrzeni) są spójne z concept probability
-# więc najpierw zmienia się rozkład, a dopiero później accuracy
 
 mean_dist_X0 = np.mean(dist_X0, axis=0)
 std_dist_X0 = np.std(dist_X0, axis=0)
@@ -94,41 +52,41 @@ mean_dist_X1 = np.mean(dist_X1, axis=0)
 std_dist_X1 = np.std(dist_X1, axis=0)
 
 
-aa.fill_between(np.arange(n_chunks-1), 
+# PLOT
+
+fig, ax = plt.subplots(1, 1, figsize=(8,4))
+
+ax.fill_between(np.arange(n_chunks-1), 
+                mean_acc-std_acc, mean_acc+std_acc, 
+                alpha=0.2, color='red',
+                lw=0)
+ax.plot(mean_acc, color='red')
+
+ax2 = ax.twinx()
+ax2.fill_between(np.arange(n_chunks-1), 
                 mean_dist_X0-std_dist_X0, mean_dist_X0+std_dist_X0, 
-                alpha=0.3, color='blue',
+                alpha=0.2, color='blue',
                 lw=0)
-aa.plot(mean_dist_X0, color='blue')
+ax2.plot(mean_dist_X0, color='blue')
 
-aa.fill_between(np.arange(n_chunks-1), 
-                mean_dist_X1-std_dist_X1, mean_dist_X1+std_dist_X1, 
-                alpha=0.3, color='green',
-                lw=0)
-aa.plot(mean_dist_X1, color='green')
+ax.spines['top'].set_visible(False)
+ax2.spines['top'].set_visible(False)
 
-aa2 = aa.twinx()
-aa2.plot(stream.concept_probabilities[::chunk_size], color='black', ls=':')
+ax.grid(ls=':')
+ax.set_xlim(0,98)
+ax.set_ylim(0.5,1)
 
-aa.spines['top'].set_visible(False)
-aa2.spines['top'].set_visible(False)
-
-aa.spines['right'].set_visible(False)
-aa2.spines['right'].set_visible(False)
-aa2.set_yticks([])
-
-aa.grid(ls=':')
-aa.set_xlim(0,99)
-
-aa.set_xlabel('chunk')
-aa.set_ylabel('distance from initial centroids')
+ax.set_xlabel('chunk')
+ax.set_ylabel('accuracy', color='r')
+ax2.set_ylabel('distance', color='b')
 
 handles, labels = plt.gca().get_legend_handles_labels()
-line1 = Line2D([0], [0], label='negative class', color='blue')
-line2= Line2D([0], [0], label='positive class', color='green')
-line3 = Line2D([0], [0], label='concept probability', color='black', ls=':')
-handles.extend([line1, line2, line3])
-aa.legend(handles=handles, frameon=False)
+line1 = Line2D([0], [0], label='Gaussian Naive Bayes', color='red')
+line2 = Line2D([0], [0], label='Distance from initial class centroid', color='blue')
+handles.extend([line1, line2])
+ax.legend(handles=handles, frameon=False, loc=4)
 
 plt.tight_layout()
 plt.savefig('foo.png')
 plt.savefig('preliminary/pre_acc.png')
+plt.savefig('preliminary/pre_acc.pdf')
