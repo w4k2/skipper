7a85868444a24129b97fd5322a2f15089230e200
Joana <asiakom24@vp.pl>
2025-06-04 13:31:01 +0200

    CD plots

--- experiment_covtype.py
+++ experiment_covtype.py
@@ -30,71 +30,67 @@ class MLPwrap:
     
 X, y = fetch_covtype(return_X_y=True)
 n_features = X.shape[1]
+classes = np.unique(y)
 
 print(X.shape)
 print(np.unique(y, return_counts=True))
 
-y[y<2] = 0
-y[y>=2] = 1
-
-# !
-# TODO ova/ovo for replications? and then wilcoxon
-
-# X = X[y<2]
-# y = y[y<2]
-
-print(np.unique(y, return_counts=True))
-
-data = np.column_stack([X,y])
-print(data.shape)
-
-np.save('covtype.npy', data)
-
 chunk_size = 500
-n_chunks = data.shape[0]//chunk_size
-
+n_chunks = X.shape[0]//chunk_size
 
 deltas = [1, 10, 20, 60]
 n_epochs = [1, 50, 50, 50]
-results = np.zeros((4, 4, n_chunks, 3))
+results = np.zeros((len(classes), 4, 4, n_chunks, 3))
+
+# OVA
+for class_id, pos_class in enumerate(classes):
+
+    y_new = np.zeros_like(y)
+    y_new[y==pos_class] = 1
+    
+    print(np.unique(y_new, return_counts=True))
+    data = np.column_stack([X,y_new])
+    np.save('covtype.npy', data)
 
 
-for d_id, d in enumerate(deltas):
-    frameworks = [
-        ContinousRebuild(partial=True, delta=d),
-        TriggeredRebuildSupervised(partial=True, delta=d),
-        TriggeredRebuildUnsupervised(partial=True, delta=d),
-        TriggeredRebuildPartiallyUnsupervised(partial=True, delta=d)
-    ]
 
-    dets = [None, 
-            DDM(drift_lvl=2.0),
-            OneClassDriftDetector(size = chunk_size,
-                                  dim = n_features,
-                                  percent = 0.8, 
-                                  nu=0.5), 
-            MD3(sigma=0.1)]
+    # experiment
+    for d_id, d in enumerate(deltas):
+        frameworks = [
+            ContinousRebuild(partial=True, delta=d),
+            TriggeredRebuildSupervised(partial=True, delta=d),
+            TriggeredRebuildUnsupervised(partial=True, delta=d),
+            TriggeredRebuildPartiallyUnsupervised(partial=True, delta=d)
+        ]
 
-    for f_id in range(4):
-        
-        clf = MLPwrap(MLPClassifier(random_state=997, 
-                                    hidden_layer_sizes=(100)), 
-                                    n_epochs=n_epochs[f_id])
-        f = frameworks[f_id]
-        det = dets[f_id]
-        
-        stream = NPYParser('covtype.npy',
-                    chunk_size=chunk_size, 
-                    n_chunks=n_chunks)
-        if f_id==0:
-            f.process(stream=stream, clf=clf)
-        else:
-            f.process(stream=stream, clf=clf, det=det)
+        dets = [None, 
+                DDM(drift_lvl=2.0),
+                OneClassDriftDetector(size = chunk_size,
+                                    dim = n_features,
+                                    percent = 0.8, 
+                                    nu=0.5), 
+                MD3(sigma=0.1)]
+
+        for f_id in range(4):
             
-        results[d_id, f_id, 1:, 0] = f.scores
-        results[d_id, f_id, :len(f.label_request_chunks), 1] = f.label_request_chunks
-        results[d_id, f_id, :len(f.training_chunks), 2] = f.training_chunks
+            clf = MLPwrap(MLPClassifier(random_state=997, 
+                                        hidden_layer_sizes=(100)), 
+                                        n_epochs=n_epochs[f_id])
+            f = frameworks[f_id]
+            det = dets[f_id]
+            
+            stream = NPYParser('covtype.npy',
+                        chunk_size=chunk_size, 
+                        n_chunks=n_chunks)
+            if f_id==0:
+                f.process(stream=stream, clf=clf)
+            else:
+                f.process(stream=stream, clf=clf, det=det)
+                
+            results[class_id, d_id, f_id, 1:, 0] = f.scores
+            results[class_id, d_id, f_id, :len(f.label_request_chunks), 1] = f.label_request_chunks
+            results[class_id, d_id, f_id, :len(f.training_chunks), 2] = f.training_chunks
 
-        print(f.label_request_chunks)
-        print(f.training_chunks)
-        np.save('results/res_covtype.npy', results)
+            print(f.label_request_chunks)
+            print(f.training_chunks)
+            np.save('results/res_covtype_ova.npy', results)
--- experiment_covtype_vis.py
+++ experiment_covtype_vis.py
@@ -3,68 +3,70 @@ import matplotlib.pyplot as plt
 
 deltas = [1, 10, 20, 60]
 
-frameworks = ['Continuous Rebuild (CR)',
-              'Triggered Rebuild Supervised (TR-S)',
-              'Triggered Rebuild Unsupervised (TR-U)',
-              'Triggered Rebuild Partially Unsupervised (TR-P)']
+frameworks = ['CR',
+              'TR-S',
+              'TR-U',
+              'TR-P']
 
-results = np.load('results/res_covtype.npy')
-n_chunks = results.shape[2]
+results = np.load('results/res_covtype_ova.npy')
+n_chunks = results.shape[3]
 
-print(results.shape) # deltas (4), frameworks (4), chunks, metrics        
-
-fig, ax = plt.subplots(4,4, figsize=(10,7), sharex=True, sharey=True)
-
-for f_id, f in enumerate(frameworks):
-    fig, ax = plt.subplots(4, 1, figsize=(6,8), sharex=True, sharey=True)
-    plt.suptitle('%s \n MLP | Oracle | 5 concept drifts' % f)
-
-
-    for d_id, d in enumerate(deltas):
-    
-        x1 = results[d_id, f_id, :, 1]
-        x2 = results[d_id, f_id, :, 2]
-        ax_twin = ax[d_id].twinx()
+print(results.shape) # classes, deltas (4), frameworks (4), chunks, metrics        
+for class_id in range(results.shape[0]):
         
-        ax_twin.scatter(
-            x1, np.full_like(x1, 0),
-            color=plt.cm.coolwarm([1.0]), s=50, alpha=1)
-        ax_twin.scatter(
-            x2, np.full_like(x2, 1),
-            color=plt.cm.coolwarm([0.1]), s=50, alpha=1)
-        ax_twin.set_ylim(-0.5,2.3)
-        
-        ax_twin.set_yticks([0,1], ['Label request', 'Training'], rotation=0)
-        colors = plt.cm.coolwarm([0.0, 1.0])
-        ax_twin.get_yticklabels()[0].set_color(colors[1])
-        ax_twin.get_yticklabels()[1].set_color(colors[0])
+    fig, ax = plt.subplots(4, 4, figsize=(12,8), sharex=True, sharey=True)
+    plt.suptitle('MLP | Covtype | class: %i' % class_id)
 
-        ax[d_id].plot(
-            np.arange(n_chunks),
-            results[d_id, f_id, :, 0],
-            color='k', lw=1)
-                
-        ax[d_id].grid(ls=':')
-        ax[d_id].set_ylabel('$\delta = %i$ \n balanced accuracy' % (d))
+    for f_id, f in enumerate(frameworks):
+        ax[0, f_id].set_title(f)
         
-        if d_id==3:
-            ax[d_id].set_xlabel('$chunk$')
+        for d_id, d in enumerate(deltas):
+        
+            x1 = results[class_id, d_id, f_id, :, 1]
+            x2 = results[class_id, d_id, f_id, :, 2]
+            ax_twin = ax[d_id, f_id].twinx()
             
-        ax[d_id].spines['top'].set_visible(False)
-        ax[d_id].spines['right'].set_visible(False)
-        ax[d_id].set_xlim(0,n_chunks)
-        
-        ax_twin.spines['top'].set_visible(False)
-        ax_twin.spines['right'].set_visible(False)
-        ax_twin.set_xlim(0,n_chunks)
-        
-        ax_twin.set_zorder(1)
-        ax[d_id].patch.set_visible(False) # hide the 'canvas'
-        ax[d_id].set_zorder(2)
+            ax_twin.scatter(
+                x1, np.full_like(x1, 0),
+                color=plt.cm.coolwarm([1.0]), s=20, alpha=1)
+            ax_twin.scatter(
+                x2, np.full_like(x2, 1),
+                color=plt.cm.coolwarm([0.1]), s=20, alpha=1)
+            ax_twin.set_ylim(-0.5,2.3)
+            
+            # ax_twin.set_yticks([0,1], ['Label request', 'Training'], rotation=0)
+            ax_twin.set_yticks([])
+            # colors = plt.cm.coolwarm([0.0, 1.0])
+            # ax_twin.get_yticklabels()[0].set_color(colors[1])
+            # ax_twin.get_yticklabels()[1].set_color(colors[0])
+
+            ax[f_id, d_id].plot(
+                np.arange(n_chunks),
+                results[class_id, d_id, f_id, :, 0],
+                color='k', lw=1)
+                    
+            ax[d_id, f_id].grid(ls=':')
+            if f_id==0:
+                ax[d_id, f_id].set_ylabel('$\delta = %i$ \n balanced accuracy' % (d))
+            
+            if d_id==3:
+                ax[d_id, f_id].set_xlabel('$chunk$')
+                
+            ax[d_id, f_id].spines['top'].set_visible(False)
+            ax[d_id, f_id].spines['right'].set_visible(False)
+            ax[d_id, f_id].set_xlim(0,n_chunks)
+            
+            ax_twin.spines['top'].set_visible(False)
+            ax_twin.spines['right'].set_visible(False)
+            ax_twin.set_xlim(0,n_chunks)
+            
+            ax_twin.set_zorder(1)
+            ax[d_id, f_id].patch.set_visible(False) # hide the 'canvas'
+            ax[d_id, f_id].set_zorder(2)
 
     plt.tight_layout()
     plt.savefig('foo.png', dpi=500)
-    plt.savefig('fig_frameworks/covtype_%i.png' % f_id)
-    plt.savefig('fig_frameworks/covtype_%i.eps' % f_id)
+    plt.savefig('fig_frameworks/covtype_c%i.png' % class_id)
+    plt.savefig('fig_frameworks/covtype_c%i.eps' % class_id)
 
     # exit()
